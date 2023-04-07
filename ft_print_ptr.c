c86b3701ce3335d1b125519ef980a24e69e84323
Taehyun Kwon <taehkwon@c2r10s7.42seoul.kr>
2023-04-07 22:43:18 +0900

    modify_u,x,X,p

--- ft_print_ptr.c
+++ ft_print_ptr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_ptr.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taehkwon <taehkwon@student.42.kr>          +#+  +:+       +#+        */
+/*   By: taehkwon <taehkwon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 03:52:41 by taehkwon          #+#    #+#             */
-/*   Updated: 2023/04/07 05:30:25 by taehkwon         ###   ########.fr       */
+/*   Updated: 2023/04/07 22:40:03 by taehkwon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,29 @@ void	ft_putptr(unsigned long long ptr, int *bytes)
 {
 	if (ptr == 0)
 	{
-		*bytes += write(1, "0x0", 3);
+		ft_putstr("0x0", bytes);
+		//*bytes += write(1, "0x0", 3);
 		return ;
 	}
 	else
 	{
-		ft_putstr("0x", bytes);
+		//ft_putstr("0x", bytes);
 		ft_put_recul(ptr, "0123456789abcdef", bytes);
 	}
 }
 
 void	ft_put_recul(unsigned long long ptr, char *hex, int *bytes)
 {
-	if (ptr >= 16)
+	char	c;
+
+	if (ptr == 0)
+		ft_putstr("0x", bytes);
+		//*bytes += write(1, "0x", 2);
+	else
 	{
-		ft_put_recul(ptr, hex[ptr/16], bytes);
-		ft_put_recul(ptr, hex[ptr%16], bytes);
+		ft_put_recul(ptr / 16, "0123456789abcdef", bytes);
+		c = hex[ptr % 16] + '0';
+		ft_putchar(c, bytes);
+		// ft_putchar(hex[ptr % 16], bytes);
 	}
-	ptr
-	ft_putchar(hex[ptr])
 }
